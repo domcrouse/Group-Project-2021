0df21b76b0ca71dd3f96b9337a8044c23ae24c30
qui16000158 <55698561+qui16000158@users.noreply.github.com>
2021-04-28 20:23:16 +0100

    Added hazards/goal scripts. + Various required components including TextMeshPro

---
+++ Assets/Scripts/Hazard.cs
@@ -0,0 +1,21 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hazard : NetworkBehaviour
+{
+    [SerializeField]
+    int damage = 25;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (isServer)
+        {
+            if (other.TryGetComponent(out IDamageable damageable))
+            {
+                damageable.TakeDamage(damage);
+            }
+        }
+    }
+}
