f693ad8d81ce8f8d062e0fd51e41c5f289fa8869
qui16000158 <55698561+qui16000158@users.noreply.github.com>
2021-04-30 14:14:21 +0100

    Added spawn points

---
+++ Assets/Scripts/SpawnSelector.cs
@@ -0,0 +1,34 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnSelector : NetworkBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Transform spawnPoint = GameObject.FindGameObjectWithTag("Spawn Point")?.transform ?? null;
+
+        if (spawnPoint)
+        {
+            CharacterController cc = GetComponent<CharacterController>();
+
+            if (cc != null)
+            {
+                cc.enabled = false;
+            }
+
+            if (isLocalPlayer) {
+                transform.position = spawnPoint.position;
+            }
+
+            Destroy(spawnPoint.gameObject);
+
+            if (cc != null)
+            {
+                cc.enabled = true;
+            }
+        }
+    }
+}
